cbdfff87c502f9036414ac7c4370103c3298d4ff
Ashen Cone <ashencone@gmail.com>
2022-01-08 14:54:55 +0530

    Perf: use textContent instead of innerHTML

--- scripts/app.js
+++ scripts/app.js
@@ -27,26 +27,26 @@ function makeCard(pokemon) {
     section.appendChild(divMoves).className = "pokemon__moves";
     let spanTitle = document.createElement("span");
     divTitle.appendChild(spanTitle).className = "pokemon__name";
-    spanTitle.innerHTML = pokemon.name;
+    spanTitle.textContent = pokemon.name;
     let spanType1 = document.createElement("span");
     divTitle.appendChild(spanType1).className = "pokemon__type";
     spanType1.className += ` pokemon__type--${pokemon.type[0].toLowerCase()}`;
-    spanType1.innerHTML = pokemon.type[0];
+    spanType1.textContent = pokemon.type[0];
     if (pokemon.type[0] != pokemon.type[1]) {
         let spanType2 = document.createElement("span");
         divTitle.appendChild(spanType2).className = "pokemon__type";
         spanType2.className += ` pokemon__type--${pokemon.type[1].toLowerCase()}`;
-        spanType2.innerHTML = pokemon.type[1];
+        spanType2.textContent = pokemon.type[1];
     }
     let divStats = document.createElement("div");
     divData.appendChild(divStats).className = "pokemon__stats";
     pokemon.baseStats.forEach((stat, id) => {
         let spanStatName = document.createElement("span");
         divStats.appendChild(spanStatName).className = "pokemon__stat-name";
-        spanStatName.innerHTML = statsName[id];
+        spanStatName.textContent = statsName[id];
         let spanStatValue = document.createElement("span");
         divStats.appendChild(spanStatValue).className = "pokemon__stat-value";
-        spanStatValue.innerHTML = stat.toString();
+        spanStatValue.textContent = stat.toString();
         let spanStatBar = document.createElement("span");
         divStats.appendChild(spanStatBar).className = "pokemon__stat-bar";
         spanStatBar.className += ` pokemon__stat-bar--${Math.floor((clamp(stat, 10, 110) - 10) / 20)}`;
@@ -56,65 +56,65 @@ function makeCard(pokemon) {
     divData.appendChild(divInfo).className = "pokemon__info";
     let spanAbilityName = document.createElement("span");
     divInfo.appendChild(spanAbilityName).className = "pokemon__info-prop";
-    spanAbilityName.innerHTML = "Ability";
+    spanAbilityName.textContent = "Ability";
     let spanAbility1 = document.createElement("span");
     divInfo.appendChild(spanAbility1).className = "pokemon__info-value";
-    spanAbility1.innerHTML = pokemon.ability[0];
+    spanAbility1.textContent = pokemon.ability[0];
     if (pokemon.ability[1] != strNull &&
         pokemon.ability[1] != pokemon.ability[0]) {
         let spanAbility2 = document.createElement("span");
         divInfo.appendChild(spanAbility2).className = "pokemon__info-value";
-        spanAbility2.innerHTML = pokemon.ability[1];
+        spanAbility2.textContent = pokemon.ability[1];
     }
     if (pokemon.hiddenAbility != strNull) {
         let spanHAbilityName = document.createElement("span");
         divInfo.appendChild(spanHAbilityName).className = "pokemon__info-prop";
-        spanHAbilityName.innerHTML = "Hidden Ability";
+        spanHAbilityName.textContent = "Hidden Ability";
         let spanHAbilityValue = document.createElement("span");
         divInfo.appendChild(spanHAbilityValue).className = "pokemon__info-value";
-        spanHAbilityValue.innerHTML = pokemon.hiddenAbility;
+        spanHAbilityValue.textContent = pokemon.hiddenAbility;
     }
     if (pokemon.item[0] != strNull || pokemon.item[1] != strNull) {
         let spanHItemName = document.createElement("span");
         divInfo.appendChild(spanHItemName).className = "pokemon__info-prop";
-        spanHItemName.innerHTML = "Held Item";
+        spanHItemName.textContent = "Held Item";
         if (pokemon.item[0] != strNull) {
             let spanItem1 = document.createElement("span");
             divInfo.appendChild(spanItem1).className = "pokemon__info-value";
-            spanItem1.innerHTML = pokemon.item[0];
+            spanItem1.textContent = pokemon.item[0];
         }
         if (pokemon.item[1] != strNull) {
             let spanItem2 = document.createElement("span");
             divInfo.appendChild(spanItem2).className = "pokemon__info-value";
-            spanItem2.innerHTML = pokemon.item[1];
+            spanItem2.textContent = pokemon.item[1];
         }
     }
     let spanEGroupName = document.createElement("span");
     divInfo.appendChild(spanEGroupName).className = "pokemon__info-prop";
-    spanEGroupName.innerHTML = "Egg Group";
+    spanEGroupName.textContent = "Egg Group";
     let spanGroup1 = document.createElement("span");
     divInfo.appendChild(spanGroup1).className = "pokemon__info-value";
-    spanGroup1.innerHTML = pokemon.eggGroup[0];
+    spanGroup1.textContent = pokemon.eggGroup[0];
     if (pokemon.eggGroup[1] != pokemon.eggGroup[0]) {
         let spanGroup2 = document.createElement("span");
         divInfo.appendChild(spanGroup2).className = "pokemon__info-value";
-        spanGroup2.innerHTML = pokemon.eggGroup[1];
+        spanGroup2.textContent = pokemon.eggGroup[1];
     }
     let spanEVYieldName = document.createElement("span");
     divInfo.appendChild(spanEVYieldName).className = "pokemon__info-prop";
-    spanEVYieldName.innerHTML = "EV Yield";
+    spanEVYieldName.textContent = "EV Yield";
     pokemon.evYield.forEach((ev, id) => {
         if (ev) {
             let spanEV = document.createElement("span");
             divInfo.appendChild(spanEV).className = "pokemon__info-value";
-            spanEV.innerHTML = `${ev} ${statsLongName[id]}`;
+            spanEV.textContent = `${ev} ${statsLongName[id]}`;
         }
     });
     let divMovesHeader = document.createElement("div");
     divMoves.appendChild(divMovesHeader).className = "pokemon__moves-header";
     let h3MovesTitle = document.createElement("h3");
     divMovesHeader.appendChild(h3MovesTitle).className = "pokemon__moves-title";
-    h3MovesTitle.innerHTML = "Learnset";
+    h3MovesTitle.textContent = "Learnset";
     let chevronDown = document.createElement("i");
     divMovesHeader.appendChild(chevronDown).className =
         "ai-chevron-down pokemon__moves-icon";
@@ -124,25 +124,25 @@ function makeCard(pokemon) {
         let h4LevelMovesName = document.createElement("h4");
         divMovesAll.appendChild(h4LevelMovesName).className =
             "pokemon__lv-moves-header";
-        h4LevelMovesName.innerHTML = "Level-up Moves";
+        h4LevelMovesName.textContent = "Level-up Moves";
         pokemon.levelUpMoves.forEach(([lv, move]) => {
             let spanLevel = document.createElement("span");
             divMovesAll.appendChild(spanLevel).className = "pokemon__lv-move-lv";
-            spanLevel.innerHTML = lv.toString();
+            spanLevel.textContent = lv.toString();
             let spanMove = document.createElement("span");
             divMovesAll.appendChild(spanMove).className = "pokemon__lv-move-name";
-            spanMove.innerHTML = move;
+            spanMove.textContent = move;
         });
     }
     if (pokemon.eggMoves.length) {
         let h4EggMovesName = document.createElement("h4");
         divMovesAll.appendChild(h4EggMovesName).className =
             "pokemon__egg-moves-header";
-        h4EggMovesName.innerHTML = "Egg Moves";
+        h4EggMovesName.textContent = "Egg Moves";
         pokemon.eggMoves.forEach((move) => {
             let spanMove = document.createElement("span");
             divMovesAll.appendChild(spanMove).className = "pokemon__egg-move-name";
-            spanMove.innerHTML = move;
+            spanMove.textContent = move;
         });
     }
     return section;
